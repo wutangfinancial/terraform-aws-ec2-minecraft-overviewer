f77fd1d30fa36a4897da9dcb6c9f1324adc0c940
Wu Tang Financial <wutangfinacial@detwa.com>
2020-06-24 18:09:53 -0400

    updates to 2.0 provider syntax

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # overviewer Web Instance
 ###############################################################################
 resource "aws_instance" "overviewer" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_minecraft_secgroups}"]
+  ami = var.my_ami
+  subnet_id = var.my_subnet
+  key_name = var.my_key
+  vpc_security_group_ids = var.my_minecraft_secgroups
   instance_type = "t2.micro"
   disable_api_termination = true
   
@@ -23,8 +23,8 @@ resource "aws_instance" "overviewer" {
 }
 
 resource "aws_route53_record" "overviewer" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "overviewer.detwah.com"
+  zone_id = var.my_route53_zone_id
+  name    = "overviewer.detwa.com"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.overviewer.public_ip}"]
--- variables.tf
+++ variables.tf
@@ -4,26 +4,26 @@ provider "aws" {
 }
 
 variable "my_ami" {
-  type    = "string"
+  type    = string
   default = "ami-ami-0e9089763828757e1"
 }
 
 variable "my_subnet" {
-  type    = "string"
-  default = "subnet-3805d964"
+  type    = string
+  default = "subnet-0050ef5ef92ba59e8"
 }
 
 variable "my_key" {
-  type    = "string"
+  type    = string
   default = "amzn-detwa-east"
 }
 
 variable "my_minecraft_secgroups" {
-  type    = "list"
-  default = ["sg-01954e4a","sg-001e78a5ce776e8f0"]
+  type    = list
+  default = ["sg-01954e4a"]
 }
 
 variable "my_route53_zone_id" {
-  type    = "string"
-  default = "Z20NBA4QJSYPCC"
+  type    = string
+  default = "Z37X3NLODX317I"
 }
