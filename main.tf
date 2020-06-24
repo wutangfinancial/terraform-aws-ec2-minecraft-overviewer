337cee9c8a6e4e54ae5b03bcfac6ac8930c3e51f
wutangfinancial <35402234+wutangfinancial@users.noreply.github.com>
2020-06-24 17:54:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "overviewer" {
   subnet_id = "${var.my_subnet}"
   key_name = "${var.my_key}"
   vpc_security_group_ids = ["${var.my_minecraft_secgroups}"]
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
   disable_api_termination = true
   
   tags {
@@ -28,7 +28,7 @@ resource "aws_instance" "overviewer" {
 
 resource "aws_route53_record" "overviewer" {
   zone_id = "${var.my_route53_zone_id}"
-  name    = "overviewer.detwa.com"
+  name    = "overviewer.detwah.com"
   type    = "A"
   ttl     = "300"
   records = ["${aws_instance.overviewer.public_ip}"]
