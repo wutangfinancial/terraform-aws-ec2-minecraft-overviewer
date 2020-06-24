01d100f02f50bf15c655c1fb6f774d52ad26a6b7
wutangfinancial <35402234+wutangfinancial@users.noreply.github.com>
2020-06-24 17:48:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 ###############################################################################
-# Concourse Web Instance
+# overviewer Web Instance
 ###############################################################################
-resource "aws_instance" "minecraft" {
+resource "aws_instance" "overviewer" {
   ami = "${var.my_ami}"
   subnet_id = "${var.my_subnet}"
   key_name = "${var.my_key}"
@@ -10,209 +10,27 @@ resource "aws_instance" "minecraft" {
   disable_api_termination = true
   
   tags {
-    Name = "concourse"
+    Name = "overviewer"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap.sh | sudo bash",
-    ]
+  #provisioner "remote-exec" {
+  #  inline = [
+  #    "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap.sh | sudo bash",
+  #  ]
     
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
+   # connection {
+   #   type     = "ssh"
+   #   user     = "ec2-user"
+   #   private_key = "${file("/home/sal/.ssh/id_rsa")}"
+   # }
+  #}
 }
 
-resource "aws_route53_record" "concourse" {
+resource "aws_route53_record" "overviewer" {
   zone_id = "${var.my_route53_zone_id}"
-  name    = "concourse.detwah.com"
+  name    = "overviewer.detwa.com"
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.concourse.public_ip}"]
+  records = ["${aws_instance.overviewer.public_ip}"]
 }
 
-###############################################################################
-# Concourse Worker Instance
-###############################################################################
-resource "aws_instance" "concourse-worker" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_concourse_worker_secgroups}"]
-  instance_type = "t2.nano"
-  disable_api_termination = true
-  
-  tags {
-    Name = "concourse-worker"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap.sh | sudo bash",
-    ]
-    
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
-}
-
-resource "aws_route53_record" "concourse-worker" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "concourse-worker.detwah.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.concourse-worker.public_ip}"]
-}
-
-###############################################################################
-# PostgresDB Instance
-###############################################################################
-resource "aws_instance" "postgresdb" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_postgresdb_secgroups}"]
-  instance_type = "t2.nano"
-  disable_api_termination = true
-  
-  tags {
-    Name = "postgresdb"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap.sh | sudo bash",
-    ]
-    
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
-}
-
-resource "aws_route53_record" "postgresdb" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "postgresdb.detwah.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.postgresdb.public_ip}"]
-}
-
-###############################################################################
-# Bastion Ring Narya
-###############################################################################
-resource "aws_instance" "narya" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_bastion_ring_secgroups}"]
-  instance_type = "t2.nano"
-  disable_api_termination = true
-  
-  tags {
-    Name = "hab-bastion-narya"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap-nostart.sh | sudo bash",
-      "nohup sudo hab sup run --permanent-peer --peer=vilya.detwah.com --peer=nenya.detwah.com &",
-    ]
-    
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
-}
-
-resource "aws_route53_record" "narya" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "narya.detwah.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.narya.public_ip}"]
-}
-
-###############################################################################
-# Bastion Ring Nenya 
-###############################################################################
-resource "aws_instance" "nenya" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_bastion_ring_secgroups}"]
-  instance_type = "t2.nano"
-  disable_api_termination = true
-  
-  tags {
-    Name = "hab-bastion-nenya"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap-nostart.sh | sudo bash",
-      "nohup sudo hab sup run --permanent-peer --peer=vilya.detwah.com --peer=narya.detwah.com &",
-    ]
-    
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
-}
-
-resource "aws_route53_record" "nenya" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "nenya.detwah.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.nenya.public_ip}"]
-}
-
-###############################################################################
-# Bastion Ring Vilya
-###############################################################################
-resource "aws_instance" "vilya" {
-  ami = "${var.my_ami}"
-  subnet_id = "${var.my_subnet}"
-  key_name = "${var.my_key}"
-  vpc_security_group_ids = ["${var.my_bastion_ring_secgroups}"]
-  instance_type = "t2.nano"
-  disable_api_termination = true
-  
-  tags {
-    Name = "hab-bastion-vilya"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl https://raw.githubusercontent.com/wutangfinancial/my_hab_bootstrap/master/bootstrap-nostart.sh | sudo bash",
-      "nohup sudo hab sup run --permanent-peer --peer=nenya.detwah.com --peer=narya.detwah.com &",
-    ]
-    
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("/home/sal/.ssh/id_rsa")}"
-    }
-  }
-}
-
-resource "aws_route53_record" "vilya" {
-  zone_id = "${var.my_route53_zone_id}"
-  name    = "vilya.detwah.com"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.vilya.public_ip}"]
-}
